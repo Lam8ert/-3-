cdc29402c925e946cfe306a5185f811fb5003f84
ycx <1627125231@qq.com>
2025-07-25 10:18:03 +0800

    变更

--- src/依赖倒转原则/依赖倒转原则.java
+++ src/依赖倒转原则/依赖倒转原则.java
@@ -6,7 +6,7 @@ public class 依赖倒转原则 {
         com.show();
     }
 }
-class Computer{//1
+class Computer{//123456
     private CPU cpu;//应该是抽象类或者接口  来接收具体类！！！！！！！！！！
     private USB usb;
     public Computer(CPU cpu,USB usb){
